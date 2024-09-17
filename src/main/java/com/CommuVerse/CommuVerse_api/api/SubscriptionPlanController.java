2faca18838f6d9691dae37db0b0be70b7dc31f0e
Matias <malcaldel1@upao.edu.pe>
2024-09-16 23:00:58 -0500

    feauture/showSubscriptionPlan

--- src/main/java/com/CommuVerse/CommuVerse_api/api/SubscriptionPlanController.java
+++ src/main/java/com/CommuVerse/CommuVerse_api/api/SubscriptionPlanController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/subscription-plans")
 @RequiredArgsConstructor
@@ -18,6 +20,23 @@ public class SubscriptionPlanController {
     @PostMapping
     public ResponseEntity<SubscriptionPlanDTO> createSubscriptionPlan(@Valid @RequestBody SubscriptionPlanDTO dto) {
         SubscriptionPlanDTO createdPlan = subscriptionPlanService.createSubscriptionPlan(dto);
-        return new ResponseEntity<>(createdPlan, HttpStatus.CREATED); // Código 201
+        return new ResponseEntity<>(createdPlan, HttpStatus.CREATED); 
+    }
+
+    // Obtener todos los planes de suscripción
+    @GetMapping
+    public ResponseEntity<List<SubscriptionPlanDTO>> getAllSubscriptionPlans() {
+        List<SubscriptionPlanDTO> plans = subscriptionPlanService.getAllSubscriptionPlans();
+        return new ResponseEntity<>(plans, HttpStatus.OK); 
+    }
+
+    // Obtener todos los planes de suscripción por ID de usuario
+    @GetMapping("{id}")
+    public ResponseEntity<List<SubscriptionPlanDTO>> getSubscriptionPlansByUserId(@PathVariable("id") Integer userId) {
+        List<SubscriptionPlanDTO> plans = subscriptionPlanService.getAllSubscriptionPlansByUserId(userId);
+        if (plans.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
+        }
+        return new ResponseEntity<>(plans, HttpStatus.OK); 
     }
 }

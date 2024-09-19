f9622b4623ef545a38be6790893c04b385e27270
Mclever2 <caguilari1@upao.edu.pe>
2024-09-19 11:31:45 -0500

    feature/Favorite

---
+++ src/main/java/com/CommuVerse/CommuVerse_api/api/FavoriteController.java
@@ -0,0 +1,33 @@
+package com.CommuVerse.CommuVerse_api.api;
+
+import com.CommuVerse.CommuVerse_api.dto.FavoriteDTO;
+import com.CommuVerse.CommuVerse_api.service.FavoriteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/favorites")
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    @Autowired
+    public FavoriteController(FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
+    }
+
+    // POST /favorites/addFavorite
+    @PostMapping("/addFavorite")
+    public ResponseEntity<FavoriteDTO> addFavorite(@RequestBody FavoriteDTO favoriteDTO) {
+        try {
+            FavoriteDTO createdFavorite = favoriteService.addFavorite(favoriteDTO.getReader().getId(), favoriteDTO.getArticle().getId());
+            return ResponseEntity.ok(createdFavorite);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+}
---
+++ src/main/java/com/CommuVerse/CommuVerse_api/model/entity/Favorite.java
@@ -0,0 +1,35 @@
+
+
+package com.CommuVerse.CommuVerse_api.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "favorites")
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "date_saved", nullable = false)
+    private LocalDateTime dateSaved;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User reader;
+
+    public Favorite() {
+    }
+
+}
+
+
+
